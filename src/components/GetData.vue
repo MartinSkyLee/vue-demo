7b1b0383d880e66a42e4ad39050dc7e83b0a3dc1
lichongyang <lichongyang@dahuasurvey.com>
2019-06-04 18:45:59 +0800

    #add Vue实例

---
+++ src/components/GetData.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "GetData",
+  data() {
+    return {
+      msg: null
+    };
+  },
+  mounted() {
+      axios
+      .get("http://10.9.53.103:5200/api/CompanyManager/GetCompanyList?Sidx=name")
+      .then(response => {
+        this.msg = response.data.data[1];//用ES6箭头函数，箭头方法可以和父方法共享变量
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+  components: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,15 @@
 <template>
   <div class="hello">
+    <input v-model="number">
+    <p>数字：{{ number }}</p>
+    <p>数字1：{{ number1 | toInt }}</p>
+    <p>数字2：{{ number2 }}</p>
+    <p>总和：{{ sum }}</p>
+    <p>{{ number }}</p>
+    <button v-on:click="plus">加1</button>
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-      <br>
-      <li>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
+    <p v-html="name"></p>
+    <a v-bind:href="link" target="_blank">百度地图</a>
   </div>
 </template>
 
@@ -49,19 +24,47 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: null
+      msg: null,
+      name: "<strong>Martin</strong>",
+      number: 0,
+      number1: 25.36,
+      number2: 10.24,
+      link: "https://map.baidu.com"
     };
+  },
+  filters: {
+    toInt(value) {
+      return parseInt(value);
+    }
+  },
+  computed: {
+    sum() {
+      return this.number1 + this.number2;
+    }
+  },
+  methods: {
+    plus() {
+      return this.number++;
+    }
+  },
+  watch: {
+    number() {
+      console.log("有变化，最新值：");
+      console.log(this.number);
+    }
   },
   mounted() {
     // var that = this //在then的内部不能使用Vue的实例化的this,因为在内部this没有被绑定。
     axios
-      .get("http://10.9.53.103:5200/api/CompanyManager/GetCompanyList?Sidx=name")
+      .get(
+        "http://10.9.53.103:5200/api/CompanyManager/GetCompanyList?Sidx=name"
+      )
       // .then(function(response) {
       //   // console.log(response.data);
       //   that.msg = response.data
       // })
       .then(response => {
-        this.msg = response.data.data[0];//用ES6箭头函数，箭头方法可以和父方法共享变量
+        this.msg = response.data.data[0]; //用ES6箭头函数，箭头方法可以和父方法共享变量
       })
       .catch(function(error) {
         console.log(error);
