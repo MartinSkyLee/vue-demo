cdd5c1c9360ba7e33e33745ad6efd227a18c3d8a
lichongyang <lichongyang@dahuasurvey.com>
2019-06-05 13:20:50 +0800

    #add 增加导航栏实现视图切换

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div class="nav">
+      <router-link to="/">主页</router-link>
+      <router-link to="/data">AXIOS</router-link>
+      <router-link to="/transition">TRANSITION</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.nav {
+  margin: 10px;
 }
 </style>
